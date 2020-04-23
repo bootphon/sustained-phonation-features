f825aa16cfac60f435e53b35804c064690f56d2e
hadware <hadwarez@gmail.com>
2020-04-23 15:13:54 +0200

    Scaffolded the feature extraction tutorial-like notebook

--- features/base.py
+++ features/base.py
@@ -5,9 +5,10 @@ import numpy as np
 
 class AudioSample:
 
-    def __init__(self, data: np.ndarray, rate: float):
+    def __init__(self, data: np.ndarray, rate: int, name: str):
         self.data = data
         self.rate = rate
+        self.name = name
 
 
 class SampleProcessor:
