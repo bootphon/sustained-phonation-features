0a76dc91ae07432076ab3b14985a492d68cdaef9
Rachine <riadrachid3@gmail.com>
2020-07-31 12:45:17 +0200

    Finished example notebook

--- features/base.py
+++ features/base.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import numpy as np
-
+import scipy.signal as sps
 
 class AudioSample:
 
@@ -12,6 +12,13 @@ class AudioSample:
 
 
 class SampleProcessor:
-
+    INPUT_SAMPLING_RATE = 44100
     def process(self, sample: AudioSample) -> Any:
         pass
+    def __call__(self, sample: AudioSample):
+        if sample.rate != self.INPUT_SAMPLING_RATE:
+            number_of_samples = round(len(sample.data) * float(self.INPUT_SAMPLING_RATE) / sample.rate)
+            data = sps.resample(sample.data, number_of_samples)
+            return self.process(AudioSample(data=data,rate=self.INPUT_SAMPLING_RATE,name=sample.name))
+        else:
+            return self.process(sample)
